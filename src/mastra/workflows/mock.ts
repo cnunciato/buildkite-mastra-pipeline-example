1bff212ffaa48744c643a742e33037ce6f10e702
Christian Nunciato <chris@nunciato.org>
2025-08-19 05:17:35 -0700

    Move into mock.ts

---
+++ src/mastra/workflows/mock.ts
@@ -0,0 +1,73 @@
+import { Pipeline as BuildkitePipeline, CommandStep, PipelineStep } from "@buildkite/buildkite-sdk";
+import * as short from "short-uuid";
+import { spawn, exec } from "node:child_process";
+
+export class Pipeline extends BuildkitePipeline {
+    private id: string;
+    private key: string;
+
+    constructor() {
+        super();
+
+        this.id = short.generate();
+        this.key = `pipeline-${this.id}-result`;
+    }
+
+    addStep(step: CommandStep) {
+        const commands = Array.isArray(step.commands) ? step.commands : [step.command];
+
+        commands.push(
+            `sleep ${Math.ceil(Math.random() * 10)}`,
+            `buildkite-agent meta-data set "${this.key}" "success"`,
+        );
+
+        return this;
+    }
+
+    async upload() {
+        return new Promise((resolve, reject) => {
+            const child = spawn("buildkite-agent", ["pipeline", "upload"], {
+                stdio: ["pipe", "pipe", "pipe"],
+            });
+
+            let output = "";
+            let error = "";
+
+            child.stdout.on("data", chunk => (output += chunk.toString()));
+            child.stderr.on("data", chunk => (error += chunk.toString()));
+            child.on("close", code =>
+                code === 0
+                    ? resolve(output.trim())
+                    : reject(new Error(error || `Exited with code ${code}`)),
+            );
+
+            child.stdin.write(this.toYAML());
+            child.stdin.end();
+        });
+    }
+
+    async complete(): Promise<"success" | "error"> {
+        while (true) {
+            try {
+                const result = await this.getMetadata();
+                return result === "success" ? "success" : "error";
+            } catch {
+                // Retry.
+            }
+            await new Promise(r => setTimeout(r, 2000));
+        }
+    }
+
+    private async getMetadata(): Promise<string> {
+        return new Promise((resolve, reject) => {
+            exec(
+                `buildkite-agent meta-data get "${this.key}"`,
+                { encoding: "utf8" },
+                (err, stdout) => {
+                    if (err) return reject(err);
+                    resolve(stdout.trim());
+                },
+            );
+        });
+    }
+}
--- src/mastra/workflows/pipeline.ts
+++ src/mastra/workflows/pipeline.ts
@@ -1,75 +1,21 @@
 import { createStep, createWorkflow } from "@mastra/core/workflows";
-import { Pipeline } from "@buildkite/buildkite-sdk";
-import { spawn, exec } from "node:child_process";
+import { Pipeline } from "./util";
 import { z } from "zod";
-
-async function getMetadata(key: string): Promise<string> {
-    return new Promise((resolve, reject) => {
-        exec(`buildkite-agent meta-data get "${key}"`, { encoding: "utf8" }, (err, stdout) => {
-            if (err) return reject(err);
-            resolve(stdout.trim());
-        });
-    });
-}
-
-async function waitForStepResult(step: number, delay = 5000): Promise<string> {
-    while (true) {
-        try {
-            const result = await getMetadata(`step${step}Result`);
-            if (result) {
-                return result;
-            }
-        } catch {
-            // Retry.
-        }
-        await new Promise(r => setTimeout(r, delay));
-    }
-}
-
-async function uploadStep(step: number, parent?: number): Promise<string> {
-    const pipeline = new Pipeline();
-
-    pipeline.addStep({
-        id: `step${step}`,
-        label: `:wave: Hi from step${step}!`,
-        commands: [
-            `echo 'Hi from step${step}!'`,
-
-            // Set the "success" of this step.
-            `sleep 10`,
-            `buildkite-agent meta-data set "step${step}Result" "success"`,
-        ],
-        depends_on: parent ? `step${parent}` : undefined,
-    });
-
-    return new Promise((resolve, reject) => {
-        const child = spawn("buildkite-agent", ["pipeline", "upload"], {
-            stdio: ["pipe", "pipe", "pipe"],
-        });
-
-        let output = "";
-        let error = "";
-
-        child.stdout.on("data", chunk => (output += chunk.toString()));
-        child.stderr.on("data", chunk => (error += chunk.toString()));
-        child.on("close", code =>
-            code === 0
-                ? resolve(output.trim())
-                : reject(new Error(error || `Exited with code ${code}`)),
-        );
-
-        child.stdin.write(pipeline.toYAML());
-        child.stdin.end();
-    });
-}
 
 const step1 = createStep({
     id: "step1",
     inputSchema: z.string(),
     outputSchema: z.string(),
     execute: async () => {
-        await uploadStep(1);
-        return await waitForStepResult(1);
+        const pipeline = new Pipeline();
+
+        pipeline.addStep({
+            label: ":wave: Hi from step1",
+            command: `echo 'Hi from step1!'`,
+        });
+
+        const result = await pipeline.complete();
+        return result;
     },
 });
 
@@ -78,11 +24,22 @@ const step2 = createStep({
     inputSchema: z.string(),
     outputSchema: z.string(),
     execute: async () => {
-        await uploadStep(2);
-        const result = await waitForStepResult(2);
+        const pipeline = new Pipeline();
+
+        pipeline.addStep({
+            label: ":wave: Hi from step2",
+            command: `echo 'Hi from step2!'`,
+        });
+
+        const result = await pipeline.complete();
 
         if (result === "success") {
-            uploadStep(4, 2);
+            const pipeline = new Pipeline();
+
+            pipeline.addStep({
+                label: ":wave: Hi from step4",
+                command: `echo 'Hi from step4!'`,
+            });
         }
 
         return result;
@@ -94,8 +51,15 @@ const step3 = createStep({
     inputSchema: z.string({}),
     outputSchema: z.string({}),
     execute: async () => {
-        await uploadStep(3);
-        return await waitForStepResult(3);
+        const pipeline = new Pipeline();
+
+        pipeline.addStep({
+            label: ":wave: Hi from step3",
+            command: `echo 'Hi from step13'`,
+        });
+
+        const result = await pipeline.complete();
+        return result;
     },
 });
 
